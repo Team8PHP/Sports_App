f4c3857ce5104639c14956f250e876d7fde339f7
alyossama <alyossama8@gmail.com>
2022-11-06 22:25:53 +0200

    League component

---
+++ src/app/components/views/league/league.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LeagueComponent } from './league.component';
+
+describe('LeagueComponent', () => {
+  let component: LeagueComponent;
+  let fixture: ComponentFixture<LeagueComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LeagueComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LeagueComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

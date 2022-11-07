15e57ee1f1c7ea05ac4d1e355849940b076214a2
Mohamed Talaat <mohamed_talaat_47@yahoo.com>
2022-11-07 14:32:36 +0200

    added matches list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MatchListComponent } from './components/match-list/match-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'matches', component: MatchListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatchListComponent } from './components/match-list/match-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchListComponent
   ],
   imports: [
     BrowserModule,

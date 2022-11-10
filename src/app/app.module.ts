ca30fea2285d6c0b168f5af23028f78da9c6e614
alyossama <alyossama8@gmail.com>
2022-11-10 21:43:03 +0200

    created general service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatchListComponent } from './components/match-list/match-list.component';
@@ -25,7 +25,8 @@ import { TopPlayersComponent } from './components/top-players/top-players.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CalendarModule 
+    CalendarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

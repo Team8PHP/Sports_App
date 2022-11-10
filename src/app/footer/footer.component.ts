5dd6986854dea3771af52b32867790c56ac3f826
Mnabil10 <nabilm604@gmail.com>
2022-11-10 10:27:02 +0200

    edit footer&footer css

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styles: [
-  ]
+  styleUrls: ['./footer.component.css']
+
 })
 export class FooterComponent implements OnInit {
 

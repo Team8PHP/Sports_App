9bb6e3ffbebe74dd5787f5be74a85e9e8d831deb
mazenkhattab97 <mazenkhattab97@hotmail.com>
2022-11-10 21:30:52 +0200

    merge 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatchListComponent } from './components/match-list/match-list.component';
-import { FavouritesComponent } from './Components/favourites/favourites.component';
-import { CalenderComponent } from './Components/calender/calender.component';
+import { FavouritesComponent } from './components/favourites/favourites.component';
+import { CalenderComponent } from './components/calender/calender.component';
 import { CalendarModule } from '@syncfusion/ej2-angular-calendars';
-import { AllLeaguesListComponent } from './Components/all-leagues-list/all-leagues-list.component';
+import { AllLeaguesListComponent } from './components/all-leagues-list/all-leagues-list.component';
+import { StandingsComponent } from './components/standings/standings.component';
+import { TopPlayersComponent } from './components/top-players/top-players.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,10 @@ import { AllLeaguesListComponent } from './Components/all-leagues-list/all-leagu
     FavouritesComponent,
     CalenderComponent,
     AllLeaguesListComponent,
-    MatchListComponent
+    MatchListComponent,
+   StandingsComponent,
+   TopPlayersComponent,
+   MatchListComponent
   ],
   imports: [
     BrowserModule,

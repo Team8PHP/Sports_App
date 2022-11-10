409b603017fb60a15a7ad7744bd51004d3ca44fc
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-10 13:44:10 +0200

    add reactive Forms Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthFormComponent } from './Components/auth-form/auth-form.component';
+import {  ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AuthFormComponent } from './Components/auth-form/auth-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

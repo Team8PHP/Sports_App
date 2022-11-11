52e485a29ad18d48531f05b9ec97b7a3496bc010
Mohamed Talaat <42847761+mohamedtalaat47@users.noreply.github.com>
2022-11-11 17:47:45 +0200

    added league-wrapper route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from './auth.guard';
 import { AuthFormComponent } from './Components/auth-form/auth-form.component';
+import { StandingsTopWrapperComponent } from './Components/standings-top-wrapper/standings-top-wrapper.component';
 import { ErrorComponent } from './views/error/error.component';
 import { FavouritesViewComponent } from './views/favourites-view/favourites-view.component';
 import { HomeViewComponent } from './views/home-view/home-view.component';
@@ -9,6 +10,7 @@ import { HomeViewComponent } from './views/home-view/home-view.component';
 const routes: Routes = [
   { path: '', component: HomeViewComponent },
   { path: 'auth', component: AuthFormComponent },
+  { path: 'league/{id}', component: StandingsTopWrapperComponent },
   { path: 'favourites', component: FavouritesViewComponent , canActivate: [AuthGuardService]}, 
   { path: '**', component: ErrorComponent }, 
 ];

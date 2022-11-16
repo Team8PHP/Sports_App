d99cab94c07afa0fba42dc4ced19814d3459517a
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-16 17:07:57 +0200

    add method to get data  from database

--- src/app/Components/news-card/news-card.component.ts
+++ src/app/Components/news-card/news-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewService } from 'src/app/Services/new.service';
 
 @Component({
   selector: 'app-news-card',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-card.component.css']
 })
 export class NewsCardComponent implements OnInit {
-
-  constructor() { }
+  otherNews:any;
+  returnNews:any;
+  constructor(private news:NewService) { }
 
   ngOnInit(): void {
   }
 
+  getOther(){
+    return this.news.GetOtherNews().subscribe(res =>{
+        this.returnNews= res
+        this.otherNews = this.returnNews.data
+        console.log(this.returnNews);
+     })
+   }
 }

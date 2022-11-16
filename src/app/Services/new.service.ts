a6a28ed2065c3da4d9c6d62fc2b0be2f1f9fa257
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-16 17:06:34 +0200

    add two methods to get news

--- src/app/Services/new.service.ts
+++ src/app/Services/new.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { baseUrl } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  GetLatestNews() {
+    return this.http.get(baseUrl+"news/latest")
+  }
+
+  GetOtherNews() {
+    return this.http.get(baseUrl+"news/other")
+  }
 }

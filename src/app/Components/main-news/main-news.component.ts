e6dcf1580d771cc6e4e9ef3408b9dbbc4d79d76d
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-16 16:56:23 +0200

    create main news component

---
+++ src/app/Components/main-news/main-news.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-news',
+  templateUrl: './main-news.component.html',
+  styleUrls: ['./main-news.component.css']
+})
+export class MainNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

4de5589f5dc3601d1909b2ce5c734575300c9683
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-17 19:17:05 +0200

    add news view component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,12 +11,14 @@ import { StandingsTopWrapperComponent } from './Components/standings-top-wrapper
 import { ErrorComponent } from './views/error/error.component';
 import { FavouritesViewComponent } from './views/favourites-view/favourites-view.component';
 import { HomeViewComponent } from './views/home-view/home-view.component';
+import { NewsViewComponent } from './views/news-view/news-view.component';
 
 const routes: Routes = [
   { path: '', component: HomeViewComponent },
   { path: 'auth', component: AuthFormComponent },
   { path: 'league/:id', component: StandingsTopWrapperComponent },
   { path: 'club/:id', component: ClubComponent },
+  { path: 'news', component: NewsViewComponent },
   { path: 'favourites', component: FavouritesViewComponent, canActivate: [AuthGuardService] },
   {
     path: 'admin', component: AdminDashboardComponent,

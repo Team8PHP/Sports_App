dc336d05bb289043f6d6ebaac659013c81dbf335
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-19 22:52:15 +0200

    add redirection to new search element

--- src/app/Components/club/club.component.ts
+++ src/app/Components/club/club.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GeneralService } from 'src/app/Services/general.service';
 import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-club',
   templateUrl: './club.component.html',
@@ -10,7 +11,7 @@ export class ClubComponent implements OnInit {
   constructor(
     private generalService: GeneralService,
     private route: ActivatedRoute
-  ) {}
+  ) { }
 
   id: any;
   club: any = [];
@@ -19,12 +20,19 @@ export class ClubComponent implements OnInit {
     // retrieve the id from the url
     this.id = this.route.snapshot.paramMap.get('id');
     this.club = this.getClub(this.id);
+    this.route.params
+      .subscribe(
+        (params: any) => {
+          this.id = params['id'];
+          this.club = this.getClub(this.id);
+        }
+      );
   }
 
   getClub(id: number) {
     return this.generalService.getClub(id).subscribe((res) => {
       this.club = res;
-      
+
       // Sorting the players by position (1:Offence, 2:Midfield, 3:Defence, 4:Goalkeeper)
       this.club.data.players.sort(function (a: any, b: any) {
         if (a.position === 'Midfield') return -1;

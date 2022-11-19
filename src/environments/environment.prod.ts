706cc42703cfbc90ccbd7e2814e88a4eeeee15be
Mohamed Talaat <42847761+mohamedtalaat47@users.noreply.github.com>
2022-11-19 15:11:42 +0200

    prod errors handled

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true
 };
+
+export const baseUrl ='https://mtalaat.net/8score-laravel/public/api/';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false
 };
 
-export const baseUrl ='http://localhost:8000/api/';
+export const baseUrl ='https://mtalaat.net/8score-laravel/public/api/';
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
